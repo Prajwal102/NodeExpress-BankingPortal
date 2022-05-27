cd61e5eac8a879dc3ccfd5478379c3b331058fd4
Prajwal <prajwal.pathak102gmail.com>
2022-05-27 18:30:01 +0530

    module 2 complete

--- src/app.js
+++ src/app.js
@@ -14,9 +14,30 @@ app.set('views',path.join(__dirname,'/views'));
 app.set('view engine','ejs');
 
 
+const accountData = fs.readFileSync('src/json/accounts.json',options={encoding:'utf8'});
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json',options={encoding:'utf8'});
+const users = JSON.parse(userData);
+console.log(users);
 app.get('/',(req,res)=>{
-    res.render('index',{title:'Index'})
+    res.render('index',{title:'Account Summary',accounts:accounts})
 });
+
+app.get('/savings',(req,res)=>{
+    res.render('account',{account:accounts.savings})
+});
+
+app.get('/credit',(req,res)=>{
+    res.render('account',{account:accounts.credit})
+});
+app.get('/checking',(req,res)=>{
+    res.render('account',{account:accounts.checking})
+});
+
+app.get('/profile',(req,res)=>{
+    res.render('profile',{user:users[0]})
+})
 
 
 app.listen(PORT,()=>{
